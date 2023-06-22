1f867034dba366824ed9166c74c98cd2dd4a0e5d
opoojkk <gotcha1874@gmail.com>
2023-06-23 07:01:09 +0800

    init repo

---
+++ app/src/main/java/com/northious/a2step/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.northious.a2step
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
