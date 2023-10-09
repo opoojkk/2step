c367cf8be02fccc943d1015fa5855120318e153d
opoojkk <gotcha1874@gmail.com>
2023-10-09 15:45:21 +0800

    feat: change setting menu. dialog to bottom navigation view.

--- app/src/main/java/org/getbuddies/a2step/ui/home/MainActivity.kt
+++ app/src/main/java/org/getbuddies/a2step/ui/home/MainActivity.kt
@@ -1,38 +1,23 @@
 package org.getbuddies.a2step.ui.home
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.ActionMode
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.drakeet.multitype.MultiTypeAdapter
-import com.google.android.material.divider.MaterialDividerItemDecoration
-import com.google.android.material.search.SearchView
-import com.leinardi.android.speeddial.SpeedDialView
-import com.permissionx.guolindev.PermissionX
 import org.getbuddies.a2step.R
 import org.getbuddies.a2step.databinding.ActivityMainBinding
-import org.getbuddies.a2step.databinding.DialogMainSettingsBinding
-import org.getbuddies.a2step.db.totp.entity.Totp
-import org.getbuddies.a2step.extends.startActivity
 import org.getbuddies.a2step.ui.base.ViewBindingActivity
-import org.getbuddies.a2step.ui.custom.TactfulDialog
-import org.getbuddies.a2step.ui.extendz.dpToPx
-import org.getbuddies.a2step.ui.home.adapter.TotpDelegate
+import org.getbuddies.a2step.ui.home.fragment.HomeFragment
+import org.getbuddies.a2step.ui.home.fragment.ProfileFragment
 import org.getbuddies.a2step.ui.home.viewModel.TotpEditViewModel
 import org.getbuddies.a2step.ui.home.viewModel.TotpViewModel
-import org.getbuddies.a2step.ui.settings.SettingsActivity
-import org.getbuddies.a2step.ui.totp.InputManualActivity
-import org.getbuddies.a2step.ui.totp.ScanTotpActivity
-import org.getbuddies.a2step.ui.utils.ScreenUtil
 import org.getbuddies.a2step.ui.utils.StatusBars
 
 
@@ -43,12 +28,25 @@ class MainActivity : ViewBindingActivity<ActivityMainBinding>() {
     private val mTotpEditViewModel by lazy {
         ViewModelProvider(this)[TotpEditViewModel::class.java]
     }
-    private val adapter: MultiTypeAdapter by lazy { MultiTypeAdapter() }
     private var actionMode: ActionMode? = null
+    private val mFragmentContainerController: FragmentContainerController by lazy {
+        FragmentContainerController().apply {
+            registerFragment(R.id.home, HomeFragment(), true)
+            registerFragment(R.id.profile, ProfileFragment())
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initViewModel()
+        registerLifecycleObserver()
+        registerTotpEditViewModelObserver()
+    }
+
+    private fun registerLifecycleObserver() {
+        lifecycle.addObserver(mFragmentContainerController)
+    }
+
+    private fun registerTotpEditViewModelObserver() {
         mTotpEditViewModel.observe(this) {
             // selected list is empty that means exit action mode
             if (it.isEmpty()) {
@@ -67,109 +65,10 @@ class MainActivity : ViewBindingActivity<ActivityMainBinding>() {
     }
 
     override fun initViews() {
-        setSupportActionBar(mBinding.searchBar)
-        initRecyclerView()
-        initDialView()
-        initSearchView()
+        initBottomNavigationView()
+        addDefaultFragment()
     }
 
-    private fun initViewModel() {
-        mTotpViewModel.totpList.observe(this) {
-            updateRecyclerView(it)
-        }
-    }
-
-    private fun initRecyclerView() {
-        adapter.run {
-            register(
-                Totp::class.java, TotpDelegate(
-                    object : TotpDelegate.EditStateListener {
-                        override fun onSelected(totp: Totp) {
-                            if (totp == Totp.DEFAULT) {
-                                return
-                            }
-                            mTotpEditViewModel.add(totp)
-                        }
-
-                        override fun onUnselected(totp: Totp) {
-                            if (totp == Totp.DEFAULT) {
-                                return
-                            }
-                            mTotpEditViewModel.remove(totp)
-                        }
-
-                        override fun onModify(totp: Totp) {
-                            startActivity(InputManualActivity::class.java) {
-                                putExtra(InputManualActivity.EXTRA_TOTP_KEY, totp)
-                            }
-                        }
-
-                        override fun onDelete(totp: Totp) {
-                            mTotpViewModel.remove(totp)
-                            adapter.notifyItemRemoved(adapter.items.indexOf(totp))
-                        }
-
-                    })
-            )
-        }
-        adapter.items = mTotpViewModel.totpList.value ?: emptyList()
-        mBinding.totpRecyclerView.adapter = adapter
-        mBinding.totpRecyclerView.addItemDecoration(
-            MaterialDividerItemDecoration(
-                this,
-                RecyclerView.VERTICAL
-            ).apply {
-                this.dividerInsetStart = 15f.dpToPx().toInt()
-                dividerInsetEnd = 20f.dpToPx().toInt()
-            }
-        )
-        mBinding.totpRecyclerView.layoutManager = LinearLayoutManager(this)
-    }
-
-    @SuppressLint("NotifyDataSetChanged")
-    private fun updateRecyclerView(totps: List<Totp>) {
-        adapter.items = totps
-        adapter.notifyDataSetChanged()
-    }
-
-    private fun initDialView() {
-        val speedDialView: SpeedDialView = mBinding.speedDial
-        // 添加子菜单
-        speedDialView.inflate(R.menu.add_totp_options)
-        speedDialView.setOnActionSelectedListener { actionItem ->
-            when (actionItem.id) {
-                R.id.fab_input_scan -> {
-                    speedDialView.close()
-                    startActivity(Intent(this, InputManualActivity::class.java))
-                }
-
-                R.id.fab_input_manual -> {
-                    speedDialView.close()
-                    checkCameraPermissionAndScan()
-                }
-            }
-            true
-        }
-    }
-
-    private fun checkCameraPermissionAndScan() {
-        if (PermissionX.isGranted(this, Manifest.permission.CAMERA)) {
-            startActivity(Intent(this, ScanTotpActivity::class.java))
-            return
-        }
-        PermissionX.init(this).permissions(Manifest.permission.CAMERA)
-            .request { allGranted, _, _ ->
-                if (allGranted) {
-                    startActivity(Intent(this, ScanTotpActivity::class.java))
-                } else {
-                    Toast.makeText(
-                        this,
-                        R.string.toast_camera_permission_denied,
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-    }
 
     override fun onResume() {
         super.onResume()
@@ -213,65 +112,81 @@ class MainActivity : ViewBindingActivity<ActivityMainBinding>() {
         actionMode = null
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_search_bar_options, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.settings -> {
-                showSettingsDialog()
-                true
-            }
-
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    private fun showSettingsDialog() {
-        val tactfulDialog = TactfulDialog<DialogMainSettingsBinding>(this)
-        tactfulDialog.setContentView(DialogMainSettingsBinding.inflate(layoutInflater))
-        tactfulDialog.getViewBinding().settingsSyncText.setOnClickListener {
-            this@MainActivity.startActivity(Intent(this, SettingsActivity::class.java))
-            tactfulDialog.dismiss()
-        }
-        tactfulDialog.setCornerRadius(24f.dpToPx())
-        tactfulDialog.setWidth((ScreenUtil.getScreenWidth() - 15f.dpToPx() * 2).toInt())
-        tactfulDialog.setAnchorView(mBinding.searchBar, offsetY = 15f.dpToPx().toInt())
-        tactfulDialog.show()
-    }
-
-    @SuppressLint("RestrictedApi")
-    private fun initSearchView() {
-        mBinding.searchView.setStatusBarSpacerEnabled(false)
-        mBinding.searchView.addTransitionListener { _, _, newState ->
-            if (newState == SearchView.TransitionState.SHOWING) {
-                StatusBars.setStatusBarAndNavigationBarColor(window, getColor(R.color.surface_on))
-            } else if (newState == SearchView.TransitionState.HIDDEN) {
-                StatusBars.setStatusBarAndNavigationBarColor(window, Color.TRANSPARENT)
-            }
-        }
-    }
-
     private fun clearRecyclerViewActionMode() {
         if (mTotpEditViewModel.isEmpty()) {
             return
         }
-        val linearLayoutManager = mBinding.totpRecyclerView.layoutManager as LinearLayoutManager
-        for (i in linearLayoutManager.findFirstVisibleItemPosition()..linearLayoutManager.findLastVisibleItemPosition()) {
-            val view = linearLayoutManager.findViewByPosition(i)
-            // 这里操作view
-            view ?: continue
-            val holder =
-                mBinding.totpRecyclerView.getChildViewHolder(view) as TotpDelegate.ViewHolder
-            val totp = holder.getTotp()
-            totp ?: continue
-            if (mTotpEditViewModel.getSelectedTotpList().contains(totp)) {
-                mTotpEditViewModel.remove(totp)
-                holder.reset()
-            }
+        mFragmentContainerController.clearRecyclerViewActionMode()
+    }
 
+    private fun initBottomNavigationView() {
+        mBinding.bottomNavigationView.setOnItemSelectedListener {
+            replaceFragmentWithId(it.itemId)
+            true
+        }
+    }
+
+    private fun replaceFragmentWithId(id: Int) {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(R.id.fragment_container_view, tryFindFragment(id))
+        }
+    }
+
+    private fun tryFindFragment(id: Int): Fragment {
+        return mFragmentContainerController.tryFindFragment(id)
+    }
+
+    private fun addDefaultFragment(id: Int = R.id.home) {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(R.id.fragment_container_view, tryFindFragment(id))
+        }
+    }
+
+    class FragmentContainerController : DefaultLifecycleObserver {
+        private val mFragmentIdHashMap: HashMap<Int, Fragment> = HashMap()
+        private var mDefaultFragmentId = 0
+        private var mCurrentFragmentId = 0
+        override fun onStart(owner: LifecycleOwner) {
+            initFragment(owner)
+        }
+
+        override fun onDestroy(owner: LifecycleOwner) {
+            mFragmentIdHashMap.clear()
+        }
+
+        private fun initFragment(owner: LifecycleOwner) {
+            if (owner !is MainActivity) {
+                return
+            }
+            mCurrentFragmentId = mDefaultFragmentId
+        }
+
+        fun clearRecyclerViewActionMode() {
+            if (mCurrentFragmentId == 0) {
+                return
+            }
+            val currentFragment = tryFindFragment(mCurrentFragmentId)
+            if (currentFragment !is HomeFragment) {
+                return
+            }
+            currentFragment.clearRecyclerViewActionMode()
+        }
+
+        fun tryFindFragment(id: Int): Fragment {
+            return mFragmentIdHashMap[id] ?: mFragmentIdHashMap[mDefaultFragmentId]!!
+        }
+
+        fun registerFragment(id: Int, fragment: Fragment, default: Boolean = false) {
+            if (default) {
+                mDefaultFragmentId = id
+            }
+            mFragmentIdHashMap[id] = fragment
+        }
+
+        fun registerFragments(map: HashMap<Int, Fragment>) {
+            mFragmentIdHashMap.putAll(map)
         }
     }
 }
---
+++ app/src/main/java/org/getbuddies/a2step/ui/home/fragment/ProfileFragment.kt
@@ -0,0 +1,35 @@
+package org.getbuddies.a2step.ui.home.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.getbuddies.a2step.R
+import org.getbuddies.a2step.databinding.FragmentProfileBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProfileFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ProfileFragment : Fragment() {
+    private lateinit var mBinding: FragmentProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = FragmentProfileBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+}
